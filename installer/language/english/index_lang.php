57b5f2c64217b3659e9ac99b3125aaf01cd26b11
Derek Stegelman <dstegelman@gmail.com>
2010-11-02 17:53:06 -0500

    update to help.  Removing unwanted installer commit.

--- installer/language/english/index_lang.php
+++
@@ -1,7 +0,0 @@
-<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
-
-$lang['header']		=	'Welcome';
-$lang['thankyou']	= 	'';
-$lang['text']		=	'Installing FotoChest is super easy.  Follow the instructions to begin!';
-$lang['step1'] 		= 'Step 1';
-$lang['link']		= 'Proceed to the first step';

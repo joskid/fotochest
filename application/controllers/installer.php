cb165cd55a29273321ab3ed146232cd1b6e6e808
Derek Stegelman <dstegelman@gmail.com>
2010-11-08 18:42:34 -0600

    Quick fix for installer.

--- application/controllers/installer.php
+++ application/controllers/installer.php
@@ -40,45 +40,47 @@ class Installer extends Controller {
 
     public function install(){
          $this->load->library('form_validation');
+         $this->load->model('User_mdl');
 
     
 
         // Create/load the tables first.
 
-        $this->load->dbforge();
-
-        //Add the columns.
-
-        $fields = array(
-                        'settingID' => array(
-                                                 'type' => 'INT',
-                                                 'auto_increment' => TRUE
-                                          ),
-                        'settingName' => array(
-                                                 'type' => 'VARCHAR',
-                                                 'constraint' => '100',
-                                          ),
-                        'settingValue' => array(
-                                                 'type' =>'VARCHAR',
-                                                 'constraint' => '300',
-                                                 'null' => TRUE
-                                                 
-                                          ),
-                       
-                );
-        $this->dbforge->addField($fields);
-        $this->dbforge->create_table($this->input->post('tablePrefix') . 'photoSettings');
+//        $this->load->dbforge();
+//
+//        //Add the columns.
+//
+//        $fields = array(
+//                        'settingID' => array(
+//                                                 'type' => 'INT',
+//                                                 'auto_increment' => TRUE
+//                                          ),
+//                        'settingName' => array(
+//                                                 'type' => 'VARCHAR',
+//                                                 'constraint' => '100',
+//                                          ),
+//                        'settingValue' => array(
+//                                                 'type' =>'VARCHAR',
+//                                                 'constraint' => '300',
+//                                                 'null' => TRUE
+//
+//                                          ),
+//
+//                );
+//        $this->dbforge->addField($fields);
+//        $this->dbforge->create_table($this->input->post('tablePrefix') . 'photoSettings');
 
 
 
         $this->load->helper('string');
         $pass = random_string('alnum', 10);
+        $this->load->library('user_lib');
 
-        $this->User_mdl->userEmail = $this->input->post('userEmail');
-        $this->User_mdl->userPassword = $pass;
-        $this->User_mdl->userFirstName = $this->input->post('userFirstName');
-        $this->User_mdl->userLastName = $this->input->post('userLastName');
-        $this->User_mdl->register();
+        $this->user_lib->userEmail = "admin@admin.com";
+        $this->user_lib->userPassword = "admin";
+        $this->user_lib->userFirstName = "admin";
+        $this->user_lib->userLastName = "admin";
+        $this->user_lib->register();
             
     }
 

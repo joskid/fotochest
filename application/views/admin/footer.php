ea9aceb31aa676c75e7c0ae7d09d4b72d4ffc101
Derek Stegelman <dstegelman@gmail.com>
2010-11-30 11:20:21 -0600

    Removing un-needed files.

--- application/views/admin/footer.php
+++
@@ -1,8 +0,0 @@
-<div class="footer clear">
-    
-    <p class="version">&copy; 2010 <a href="http://fotochest.com" target="_blank">FotoChest</a> Version <?php echo getVersion(); ?></p>
-  
-</div>
-
-</div>
-</body>

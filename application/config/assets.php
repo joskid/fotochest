de5c43dd9c547b48a7472e7356cb19d7fadc3820
Derek Stegelman <dstegelman@gmail.com>
2010-12-01 16:19:55 -0600

    adding nice edit.

---
+++ application/config/assets.php
@@ -0,0 +1,78 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Asset Directory
+|--------------------------------------------------------------------------
+|
+| Relative to root of your web app.
+| 
+|
+|	/assets/
+|
+*/
+
+$config['asset_dir'] = "assets";
+
+/*
+|--------------------------------------------------------------------------
+| Base Javascript Directory
+|--------------------------------------------------------------------------
+|
+| This path is relative to the asset directory so if you use "javascript"
+| then the app will build "assetdir/javascript"
+|
+|	Example: "javascript"
+|
+*/
+
+$config['javascript_dir'] = "javascript";
+
+/*
+|--------------------------------------------------------------------------
+| CSS Directory
+|--------------------------------------------------------------------------
+|
+| This path is relative to the asset directory so if you use "css"
+| then the app will build "assetdir/css"
+|
+|	Example: "css"
+|
+*/
+
+$config['css_dir'] = "css";
+
+/*
+|--------------------------------------------------------------------------
+| Admin Asset Directory
+|--------------------------------------------------------------------------
+|
+| Relative to root of your web app.
+|
+|
+|	Example: "administration"
+|
+*/
+
+$config['admin_asset_dir'] = "assets/admin";
+
+/*
+|--------------------------------------------------------------------------
+| Admin CSS Directory
+|--------------------------------------------------------------------------
+|
+| This path is relative to the admin asset directory so if you use "css"
+| then the app will build "assetdir/css"
+|
+|	Example: "administration/css"
+|
+*/
+
+$config['admin_css_dir'] = "css";
+
+
+
+
+
+/* End of file config.php */
+/* Location: ./application/config/config.php */
--- application/config/versioning.php
+++ application/config/versioning.php
@@ -1,4 +1,4 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
-$config['versionNumber'] = "RC 1.5";
+$config['versionNumber'] = "Alpha 1 - 1.5";

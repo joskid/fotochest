b6115b4cda56396db87b7a57a600d1f56c3dd2dd
Derek Stegelman <dstegelman@gmail.com>
2011-02-07 22:14:59 -0600

    Removing redundant code.

--- application/models/user_mdl.php
+++ application/models/user_mdl.php
@@ -34,87 +34,16 @@ class User_mdl extends CoreModel {
     public $userDateCreated;
     public $userTable;
 
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct();
         $this->userTable = $this->config->item('userTable');
         $this->_table = $this->config->item('userTable');
     }
 
-    function read($userID = NULL)
+    function getFirstName()
     {
-        show_error('read from user model is depreciated, use core instead.');
-        log_message('ERROR', 'read from user model is depreciated, use core instead.');
-//        if($userID == NULL)
-//        {
-//            $readData = $this->db->get($this->userTable);
-//        }
-//        else
-//        {
-//            $readData = $this->db->get_where($this->userTable, array('userID'=>$userID));
-//        }
-//        return $readData;
-    }
-
-    public function readEmail($userEmail)
-    {
-        show_error('readEmail from user model is depreciated, use core instead.');
-        log_message('ERROR', 'readEmail from user model is depreciated, use core instead.');
-//        $select = "SELECT * FROM $this->userTable WHERE userEmail = '$userEmail'";
-//        $readData = $this->db->query($select);
-//        return $readData;
-    }
-    
-    function update($data, $id)
-    {
-        show_error('update from user model is depreciated, use core instead.');
-        log_message('ERROR', 'udpate from user model is depreciated, use core instead.');
-        //$this->db->where('userID', $id);
-        //$this->db->update($this->userTable, $data);
-    }
-    
-    // There is no save users..???
-    
-    function saveUser()
-    {
-        show_error('saveUser from user model is depreciated, use core instead.');
-        log_message('ERROR', 'saveUser from user model is depreciated, use core instead.');
-//    	$this->load->library('encrypt');
-//    	$data = array('userEmail'=>$this->userEmail,
-//    				  'userPassword'=>$this->encrypt->encode($this->userPassword),
-//    				  'userFirstName'=>$this->userFirstName,
-//    				  'userLastName'=>$this->userLastName);
-//    	$this->update($data, $this->userUserID);
-    }
-
-    function delete($userID)
-    {
-        show_error('delete from user model is depreciated, use core instead.');
-        log_message('ERROR', 'delete from user model is depreciated, use core instead.');
-
-        //$this->db->delete($this->userTable, array('userID'=>$userID));
-    }
-
-
-    public function getUserIDFromEmail($email){
-        show_error('getUserIDFromEmail from user model is depreciated, use core instead.');
-        log_message('ERROR', 'getUserIDFromEmail from user model is depreciated, use core instead.');
-//        $select = "SELECT userID FROM $this->userTable WHERE userEmail = '$email'";
-//        log_message('info', 'User_mdl::getUserIDUsername() is executing a query ' . $select);
-//        $dump = $this->db->query($select);
-//        foreach($dump->result() as $row){
-//            $userID = $row->userID;
-//            log_message('debug', 'User_mdl::getUserIDUsername() has fetched the userid ' . $userID);
-//        }
-//        return $userID;
-    }
-
-    function getFirstName(){
         $this->userUserID = $this->session->userdata('userid');
-        
-//        $getName = "SELECT userFirstName
-//        FROM $this->userTable
-//        WHERE userID = $this->userUserID LIMIT 1";
-
         $this->db->select('*');
         $this->db->from($this->userTable);
         $this->db->where('id', $this->userUserID);

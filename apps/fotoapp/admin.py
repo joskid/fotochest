094fad9d4236f3e1195f5f70d9a3be20b0413e67
Derek Stegelman <dstegelman@gmail.com>
2011-10-24 21:25:30 -0500

    Remove fotoapp

--- apps/fotoapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from fotoapp.models import *
-
-admin.site.register(Album)
-admin.site.register(Photo)
-
-
-
--- apps/fotoapp/templates/fotoapp/oldies/admin/modals/movePhotoModal.php
+++
@@ -1,8 +0,0 @@
-<?php echo js('fotochest'); ?>
-<div class="modal">
-    <h1>Move to Album</h1>
-    <?php echo getAlbumDropdownList(FALSE); ?>
-    <input type="hidden" name="photoID" id="photoID" value="<?php echo $photoID; ?>" />
-    <a href="#" class="button moveToAlbum"><span>Move</span></a>
-
-</div>

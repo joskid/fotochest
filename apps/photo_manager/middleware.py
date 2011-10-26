4334c849100bf2a9495adc40ee6ca1187bcde068
Derek Stegelman <dstegelman@gmail.com>
2011-10-25 22:29:54 -0500

    Notes.

--- apps/photo_manager/middleware.py
+++ apps/photo_manager/middleware.py
@@ -7,3 +7,29 @@ class SWFUploadMiddleware(object):
         if (request.method == 'POST') and (request.path == reverse('photo_manager.views.myFileHandler')) and \
                 request.POST.has_key(settings.SESSION_COOKIE_NAME):
             request.COOKIES[settings.SESSION_COOKIE_NAME] = request.POST[settings.SESSION_COOKIE_NAME]
+            
+
+# URLS if enable multi user
+## --> For User
+# User PhotoStream
+# User Albums
+# User Album
+# User PhotoStream Slideshow
+# User Album Slideshow
+# Individual photo
+# mAp
+# Upload
+
+
+# Public Photo Stream
+# Public Slideshow Photo Stream
+
+# Everyones MAp
+
+# If not multiuser.
+## Stream
+## Albums
+## Individual Photo
+## Map
+## Upload
+## Homepage is stream

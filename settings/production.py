e2511165fc7ade4552607655db06215e20563e24
unknown <Derek@Derek-PC.(none)>
2011-11-04 14:46:20 -0500

    updates from prod branch

--- settings/production.py
+++ settings/production.py
@@ -67,7 +67,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    #'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'grappelli',
     'django.contrib.admin',
     'tastypie',

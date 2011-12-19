7379aa93cdf461076ff3c387d0c519ee947b343f
Derek Stegelman <dstegelman@gmail.com>
2011-12-19 07:43:48 -0600

    Clean up of URLS #56

--- urls/local.py
+++ urls/local.py
@@ -3,13 +3,16 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 admin.autodiscover()
 from django.conf import settings
-
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+# This maps static files dirs to URLS.
 urlpatterns = staticfiles_urlpatterns() + patterns('',
 
+    # Admin URLS.
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
+    
+    # Auth Views
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': '%s/login.html' % settings.ACTIVE_THEME}),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^accounts/redirect/$', 'profiles.views.redirect_home'),
@@ -18,9 +21,14 @@ urlpatterns = staticfiles_urlpatterns() + patterns('',
     url(r'^accounts/profiles/(?P<username>[-\w]+)/$', 'profiles.views.view_profile'),
     url(r'^accounts/profile/edit/$', 'profiles.views.edit_profile'),
 
+    #API Docs
+    url(r'^docs/', 'api_docs.urls'),
     
+    # Media URLs.  
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': './uploads'}),
+    
+    # Send all remaming URLS to the App.
     url(r'^', include('photo_manager.urls')),
 )
 
--- urls/production.py
+++ urls/production.py
@@ -4,11 +4,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     
+    # Admin URLS
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': '/home/dstegelman/static/fotochest'}),
-    url(r'^map/', include('locations.urls')),
+
+    # Auth Views   
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^accounts/redirect/$', 'profiles.views.redirect_home'),
@@ -16,7 +16,11 @@ urlpatterns = patterns('',
     url(r'^accounts/register/part_two/(?P<username>[-\w]+)/$', 'profiles.views.register_part_two'),
     url(r'^accounts/profiles/(?P<username>[-\w]+)/$', 'profiles.views.view_profile'),
     url(r'^accounts/profile/edit/$', 'profiles.views.edit_profile'),
+    
+    # API Docs
     url(r'^docs/', 'api_docs.urls'),
+    
+    # ALL Others to App.
     url(r'^', include('photo_manager.urls')),
     
     

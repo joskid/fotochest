590c262929ba87ad27914f65be8037364b450e14
Derek Stegelman <dstegelman@gmail.com>
2011-12-19 07:46:22 -0600

    Fixes for settings #56

--- settings/production_example.py
+++ settings/production_example.py
@@ -1,16 +1,17 @@
 from settings.common import *
-
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 THUMBNAIL_DEBUG = DEBUG
-## Database Configuration
 
+
+## Database Configurations
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'NAME': SITE_ROOT + '/dev.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -18,52 +19,48 @@ DATABASES = {
     }
 }
 
-
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'uploads')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
+STATIC_ROOT = ''
+
 
 STATICFILES_DIRS = (
     os.path.join(SITE_ROOT, 'themes/default/static'),
+    os.path.join(SITE_ROOT, 'static'),
 )
-
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-DOMAIN_STATIC = ''
-
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = ''
+ADMIN_MEDIA_PREFIX = '%sgrappelli/' % STATIC_URL
 
-ROOT_URLCONF = 'urls.production'
-
-PHOTO_DIRECTORY = ""
-
+PHOTO_DIRECTORY = os.path.join(SITE_ROOT, 'uploads/images')
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    "",
-    
-    # We have added a "Master" template directory here, and below if you choose, add directories for each specific app.
-    #"./newapp/templates"
+    os.path.join(SITE_ROOT, 'themes/default/templates'),
+    os.path.join(SITE_ROOT, 'templates'),
 )
+
+DOMAIN_STATIC = '/static/'
+
+
+
+ROOT_URLCONF = 'urls.local'
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -77,15 +74,11 @@ INSTALLED_APPS = (
     'tastypie',
     'photo_manager',
     'api_docs',
-    # Everyone should be using south.  Seriously.
     'south',
     'sorl.thumbnail',
-    #'photo_admin',
-    'locations',
-    'profiles',
     'djcelery',
     'djkombu',
-    #'tagging',
+    'locations',
+    'profiles',
 
 )
-

a763acd0dd132e0ee8810a5e8d40e00438f9ee91
Derek Stegelman <dstegelman@gmail.com>
2012-01-27 09:36:02 -0600

    Adding changes for setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='fotochest',
+      version=version,
+      description="Photo sharing application",
+      long_description=open("README.md", "r").read(),
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Environment :: Web Environment",
+          "Intended Audience :: End Users/Desktop",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+          ],
+      keywords='photos',
+      author='Derek Stegelman',
+      author_email='dstegelman@gmail.com',
+      url='http://github.com/fotochest/fotochest',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+    )
